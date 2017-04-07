38d94d26a6f8685c6aac0025293462845129b1b2
Jonas <josl@dhi-gras.com>
2017-04-07 15:30:47 +0200

    initial commit

---
+++ sen2indices/qgis_sen2indices.py
@@ -0,0 +1,27 @@
+#Definition of inputs and outputs
+#==================================
+##Sentinel 2 Indices=name
+##Sentinel tools=group
+##ParameterRaster|input|Input Reflectance Stack|False
+##OutputDirectory|outputDirectory|Folder to save the stack of Indices
+#OutputRaster|output|Name for Index Stack
+ 
+# Call the function for Sentinel 2 index calculation
+#==================================
+
+import os
+import sys
+
+# First include the path to the function library  in the systme path so QGIS can find the function
+here = os.path.dirname(scriptDescriptionFile)
+if here not in sys.path:
+    sys.path.append(here)
+
+# Import the function
+import sen2index
+
+# Apply the function
+print 'Starting index calculation...'
+sen2index.sen2index(input, outputDirectory)
+print 'Finished writing to disk...'
+    
