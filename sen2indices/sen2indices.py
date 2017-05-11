b9772ab613d2cccd91411ccdbd2060ec2eeb3413
Jonas <josl@dhi-gras.com>
2017-05-11 10:02:58 +0200

    cleanup

--- sen2indices/sen2indices.py
+++ sen2indices/sen2indices.py
@@ -1,6 +1,5 @@
-
 # function for calculating a selction of Sentinel 2 Spectral indices
-# Kenneth Grogan 3/11/2016 
+# Kenneth Grogan 3/11/2016
 
 import os
 import gdal
@@ -8,76 +7,66 @@ import numpy as np
 
 def standard_index(band1, band2):
     """Function for standard index calculation"""
-    #idx = (band1 - band2)/(band1 + band2)
     idx = ((band1 - band2)/(band1 + band2))*10000
-    
     return idx
-    
+
 def extract_band(stack, bnd_num):
     """Function to extract single bands from stack; stack = input stack, bnd_num = the band number to extract"""
     b = stack.GetRasterBand(bnd_num)
     band = b.ReadAsArray().astype(np.float32)
-    
     return band
 
 
 def calc_index(stack, bnd_num1, bnd_num2):
     """ Function to calculate an index; stack = input stack, bnd_numx = the band number in the stack"""
-    
     band1 = extract_band(stack, bnd_num1)
     band2 = extract_band(stack, bnd_num2)
-    
     any_index = standard_index(band1, band2)
-    
     return any_index
 
 
 def sen2index(inRst, outDir):
     """
     Main function for calculating a selction of Sentinel 2 Spectral indices
-    
-    
-    
     """
     stk = gdal.Open(inRst)
-    
+
     # get raster specs
     xsize = stk.RasterXSize
     ysize = stk.RasterYSize
     proj = stk.GetProjection()
     geotransform = stk.GetGeoTransform()
-    
+
     # calculate indices: these indices were chosen based on variable importance ranking from Random Forest classification
     # calc ndvi
     ndvi_b8_b4 = calc_index(stk, 7, 3)
-    
+
     # calc ndvi using 8a
     ndvi_b8a_b4 = calc_index(stk, 8, 3)
-    
+
     # calc red edge ndi b8a_b5
     re_ndi_b8a_b5 = calc_index(stk, 8, 4)
-    
+
     # calc red edge ndi b8a_b6
     re_ndi_b8a_b6 = calc_index(stk, 8, 5)
-    
+
     # calc red edge ndi b6_b5
     re_ndi_b6_b5 = calc_index(stk, 5, 4)
-    
+
     # calc red edge ndi b7_b5
     re_ndi_b7_b5 = calc_index(stk, 6, 4)
-    
+
     # calc NDII
     ndii_b8a_b11 = calc_index(stk, 8, 9)
-    
+
     # calc NDMI
     ndwi_b8a_b12 = calc_index(stk, 8, 10)
-    
+
     # Stack and write to disk
     # get base filename and combine with outpath
     sName = os.path.splitext(os.path.basename(inRst))[-2]
-    stkPath = os.path.join(outDir, sName +'_indices.tif')
+    stkPath = os.path.join(outDir, sName + '_indices.tif')
     drv = gdal.GetDriverByName('GTiff')
-    #outTif = drv.Create(stkPath, xsize, ysize, 8, gdal.GDT_Float32)
     outTif = drv.Create(stkPath, xsize, ysize, 8, gdal.GDT_Int16)
     outTif.SetProjection(proj)
     outTif.SetGeoTransform(geotransform)
