8b14c705a3b860d82819c635acd2d3d1ddcdd293
Nicolai <nith@dhigroup.com>
2020-06-17 14:50:41 +0200

    updated

--- unzip/S2_unzip.py
+++ unzip/S2_unzip.py
@@ -1,18 +1,32 @@
-#Definition of inputs and outputs
-#==================================
-##Sentinel Tools=group
-##Unzip Sentinel-2 data=name
-##ParameterFile|inFile|zipped file|False|False|zip
-##OutputDirectory|processingDir|Directory to unzip data to
-
 import os
 import glob
 import zipfile
+from qgis.processing import alg
 
-def unzip(src_file, dst_dir):
-    with zipfile.ZipFile(src_file) as zf:
-        zf.extractall(u'\\\\?\\' + dst_dir)
+@alg(
+    name="unzipsentinel2data",
+    label=alg.tr("Unzip Sentinel-2 data"),
+    group="sentineltools",
+    group_label=alg.tr("Sentinel Tools"),
+)
+@alg.input(
+    type=alg.FILE,
+    name="inFile",
+    label="zipped file",
+    behavior=0,
+    optional=False,
+    fileFilter="zip",
+)
+@alg.input(type=alg.FILE_DEST, name="processingDir", label="Directory to unzip data to")
+def unzipsentinel2data(instance, parameters, context, feedback, inputs):
+    """ unzipsentinel2data """
+    inFile = instance.parameterAsString(parameters, 'inFile', context)
+    processingDir = instance.parameterAsString(parameters, 'processingDir', context)
+    
+    def unzip(src_file, dst_dir):
+        with zipfile.ZipFile(src_file) as zf:
+            zf.extractall(u'\\\\?\\' + dst_dir)
 
-feedback.pushConsoleInfo('Starting unzip')
-unzip(inFile, processingDir)
-feedback.pushConsoleInfo('Unzip finished...')
+    feedback.pushConsoleInfo('Starting unzip')
+    unzip(inFile, processingDir)
+    feedback.pushConsoleInfo('Unzip finished...')
